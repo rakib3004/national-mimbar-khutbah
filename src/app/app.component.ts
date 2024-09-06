bb7368141466dfb6b1c3c911d86d50e1f403cdde
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-09-06 17:27:47 +0600

    complete initial part of bloody july calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'BloodyJuly';
+  today: Date;
+  customDate: string;
+
+  constructor() { 
+    this.today = new Date();
+    this.customDate = this.getCustomDate();
+  }
+
+  ngOnInit(): void {}
+
+  getCustomDate(): string {
+    const currentDate = new Date();
+    const startOfCustomCalendar = new Date(2024, 6, 31); // July 31, 2024
+    const timeDiff = currentDate.getTime() - startOfCustomCalendar.getTime();
+    const customDaysPassed = Math.floor(timeDiff / (1000 * 3600 * 24));
+    return `July ${31 + customDaysPassed}, 2024`;
+  }
 }
