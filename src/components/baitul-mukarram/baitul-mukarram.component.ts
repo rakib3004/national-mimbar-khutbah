7a458d047f0b036a680615346a3959e74ed1ea43
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-12-06 15:29:14 +0600

    comple 6 jummah boyan

--- src/components/baitul-mukarram/baitul-mukarram.component.ts
+++ src/components/baitul-mukarram/baitul-mukarram.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './baitul-mukarram.component.css'
 })
 export class BaitulMukarramComponent {
-
+/*,
+    {
+        "id": "20e5de2a-ebaf-432c-bab7-781b16b8bd39",
+        "no": 7,
+        "date": "৬ ডিসেম্বর ২০২৪",
+        "title": "৭ম খুতবা",
+        "description": "",
+        "speaker": "মুফতি আব্দুল মালেক হাফিযাহুল্লাহ",
+        "copywriter": "মুহাম্মাদ লুতফেরাব্বী আফনান আযহারী"
+    }*/
 }
